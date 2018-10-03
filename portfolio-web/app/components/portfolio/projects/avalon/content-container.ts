ce049f182d35eee16d5c92c3515a319934fefd4d
cptran777 <cptran14@gmail.com>
2018-10-02 22:10:31 -0700

    Setup avalon page

--- portfolio-web/app/components/portfolio/projects/avalon/content-container.ts
+++ portfolio-web/app/components/portfolio/projects/avalon/content-container.ts
@@ -1,5 +1,6 @@
 import BaseContentContainer from 'portfolio-web/components/base-content-container';
+import { imageRoot } from 'portfolio-web/data/constants/asset-paths';
 
 export default class AvalonContentContainer extends BaseContentContainer {
-  // normal class body definition here
+  imageRoot = imageRoot;
 };
