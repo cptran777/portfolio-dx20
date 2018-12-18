9f4e3816bc3c78907c37e52748bd08c3262c9735
Charlie Tran <cptran14@gmail.com>
2018-12-17 23:44:33 -0600

    Complete avalon page for portfolio

--- portfolio-web/app/components/portfolio/projects/avalon/content-container.ts
+++ portfolio-web/app/components/portfolio/projects/avalon/content-container.ts
@@ -1,6 +1,25 @@
 import BaseContentContainer from 'portfolio-web/components/base-content-container';
-import { imageRoot } from 'portfolio-web/data/constants/asset-paths';
+import { imageRoot, logoImageRoot } from 'portfolio-web/data/constants/asset-paths';
+
+const skills = [
+  { name: 'Javascript', asset: 'js.png' },
+  { name: 'Redux', asset: 'redux.png' },
+  { name: 'React', asset: 'react.png' },
+  { name: 'Three.js', asset: 'threejs.png' },
+  { name: 'VR', asset: 'vr.png' },
+  { name: 'Java', asset: 'java.png' },
+  { name: 'Node', asset: 'node.png' },
+  { name: 'Express', asset: 'express.png' },
+  { name: 'PostgreSQL', asset: 'postgres.png' },
+  { name: 'WebSockets', asset: 'websocket.png' },
+  { name: 'WebRTC', asset: 'webrtc.png' }
+].map(skill => ({
+  ...skill,
+  asset: `${logoImageRoot}${skill.asset}`
+}));
 
 export default class AvalonContentContainer extends BaseContentContainer {
   imageRoot = imageRoot;
+
+  techStack = skills;
 };
